266d2a9117e1c96900dce09bab58bf65aaaba116
johan0A <arnaudchesse@outlook.com>
2024-03-02 22:04:58 +0100

    Fix insert_in_quadtree function to correctly assign childrens to nodes

--- main.c
+++ main.c
@@ -49,13 +49,14 @@ static void expand_quadtree(struct QuadTree* quadTree, int quadrant) {
 }
 
 void insert_in_quadtree(struct QuadTree* quadTree, void* leaf, int positionX, int positionY) {
+    // qudrant info:
     while (true) {
         int overMaxX = (quadTree->posMax[0] < positionX) + (quadTree->posMin[0] > positionX * 2);
         int overMaxY = (quadTree->posMax[1] < positionY) + (quadTree->posMin[1] > positionY * 2);
         if (overMaxX || overMaxY) {
             if (quadTree->height == -1){
-                quadTree->posMax[0] = positionX;
-                quadTree->posMax[1] = positionY;
+                quadTree->posMax[0] = positionX + 1;
+                quadTree->posMax[1] = positionY + 1;
                 quadTree->posMin[0] = positionX;
                 quadTree->posMin[1] = positionY;
                 quadTree->height = 1;
@@ -64,11 +65,13 @@ void insert_in_quadtree(struct QuadTree* quadTree, void* leaf, int positionX, in
             }
             expand_quadtree(quadTree, (overMaxX-1) * 2 + (overMaxY-1));
             break;
+        } else {
+            break;
         }
     }
 
     struct Node* currentNode = quadTree->root;
-    int height = quadTree->height;
+    int height = quadTree->height - 1;
     int currentX = quadTree->posMin[0];
     int currentY = quadTree->posMin[1];
     while (height > 0) {
@@ -87,6 +90,19 @@ void insert_in_quadtree(struct QuadTree* quadTree, void* leaf, int positionX, in
         }
         currentNode = (struct Node*)currentNode->childrens[quadrant];
         height--;
+    }
+    if (positionX > currentX) {
+        if (positionY > currentY) {
+            ((struct Node*)currentNode)->childrens[3] = leaf;
+        } else {
+            ((struct Node*)currentNode)->childrens[1] = leaf;
+        }
+    } else {
+        if (positionY > currentY) {
+            ((struct Node*)currentNode)->childrens[2] = leaf;
+        } else {
+            ((struct Node*)currentNode)->childrens[0] = leaf;
+        }
     }
 }
 
@@ -108,6 +124,11 @@ static void _show_quadtree_as_tree(struct QuadTree* quadTree, struct Node* curre
             printf("Leaf %d: ", i);
             printLeaf(currentNode->childrens[i]);
             printf("\n");
+        } else {
+            for(int j = 0; j < quadTree->height - height + 1; j++) {
+                printf("  ");
+            }
+            printf("Node %d\n", i);
         }
     }
     return;
@@ -146,6 +167,11 @@ int main(int argc, char const *argv[]) {
     leaf = createLeaf(1, 1, 1);
     insert_in_quadtree(quadTree, leaf, 1, 1);
     show_quadtree_as_tree(quadTree, printLeaf);
+    leaf = createLeaf(2, 2, 2);
+    insert_in_quadtree(quadTree, leaf, 2, 2);
+    show_quadtree_as_tree(quadTree, printLeaf);
+    leaf = createLeaf(1, 50, 50);
+    insert_in_quadtree(quadTree, leaf, 50, 50);
+    show_quadtree_as_tree(quadTree, printLeaf);
     return 0;
 }
-
